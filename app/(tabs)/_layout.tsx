61315ad1cf8e11a2b6597f8ea003ecc6b77ac93c
nayawakim <naya.wakim.1@ens.etsmtl.ca>
2025-11-29 18:39:06 -0500

    nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,69 @@
+// app/(tabs)/_layout.tsx
+import { HapticTab } from "@/components/haptic-tab";
 import { Tabs } from "expo-router";
 import React from "react";
-
-import { HapticTab } from "@/components/haptic-tab";
-import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Image } from "react-native";
 
 export default function TabLayout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#000000",
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarStyle: {
+          backgroundColor: "#1A1A1A",
+          borderTopWidth: 0,
+          height: 70,
+        paddingTop: 5,       
+        paddingBottom: 5,
+        },
+        tabBarShowLabel: true,
+        tabBarLabelStyle: {
+          fontSize: 12,
+          marginBottom: 6,
+        },
+tabBarIcon: ({ focused }) => {
+  let icon;
+  let size = 32; // taille par défaut
+
+  if (route.name === "map") {
+    icon = focused
+      ? require("../../assets/icons/logoMaps.png")
+      : require("../../assets/icons/logoMapsgris.png");
+
+    size = 45; // 👈 taille spéciale pour MAP
+  }
+
+  if (route.name === "chat") {
+    icon = focused
+      ? require("../../assets/icons/logoChat.png")
+      : require("../../assets/icons/logoChatgris.png");
+
+    size = 32; // 👈 taille normale pour CHAT
+  }
+
+  return (
+    <Image
+      source={icon}
+      style={{
+        width: size,
+        height: size,
+        resizeMode: "contain",
       }}
+    />
+  );
+},
+
+      })}
     >
-      {/* --- Onglet MAP --- */}
       <Tabs.Screen
         name="map"
-        options={{
-          tabBarIcon: () => (
-            <IconSymbol size={28} name="map.fill" color={"#000"} />
-          ),
-        }}
+        options={{ title: "map" }}
       />
-
-      {/* --- Onglet CAMERA --- */}
       <Tabs.Screen
-        name="camera"
-        options={{
-          tabBarIcon: () => (
-            <IconSymbol size={28} name="camera.fill" color={"#000"} />
-          ),
-        }}
+        name="chat"
+        options={{ title: "chat" }}
       />
     </Tabs>
   );
 }
-
