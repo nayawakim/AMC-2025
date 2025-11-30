c9a1fc7bc5e4ee285464ed4b713ee31265d61b68
nayawakim <naya.wakim.1@ens.etsmtl.ca>
2025-11-29 21:45:51 -0500

    Revert "Merge branch 'main' of https://github.com/nayawakim/AMC-2025"
    This reverts commit 551cad2b83af19917a5c891353decc199a6e7cfe, reversing
    changes made to 1e8dc52ac68cf6c8b05527dbc1e86c1c9162d371.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,11 @@
-import { ThemeProvider, useTheme } from "@/lib/theme-context";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "./global.css";
+import { Slot } from "expo-router";
+import React from "react";
 
+// 👇 Désactive le router-debug-nav en bas et fixe la page d'entrée
 export const unstable_settings = {
-    anchor: "(tabs)",
+  initialRouteName: "map",
 };
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-    unsavedChangesWarning: false,
-});
-
 export default function RootLayout() {
-    return (
-        <ConvexProvider client={convex}>
-            <ThemeProvider defaultTheme="system">
-                <AppContent />
-            </ThemeProvider>
-        </ConvexProvider>
-    );
-}
-
-function AppContent() {
-    const { activeTheme } = useTheme();
-    return (
-        <>
-            <Stack
-                screenOptions={{
-                    contentStyle: activeTheme,
-                }}
-            >
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen
-                    name="modal"
-                    options={{ presentation: "modal", title: "Modal" }}
-                />
-            </Stack>
-            <StatusBar style="auto" />
-        </>
-    );
+  return <Slot />;
 }
--- convex/places.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "./_generated/server";
-
-export const getPlaces = query({
-    args: {},
-    handler: async (ctx) => {
-        const places = await ctx.db.query("places").collect();
-        return places;
-    },
-});
